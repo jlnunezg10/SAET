6429a59c319f9c4cdda4c5195889ec4bb6e5ae23
Jose Nunez <juniorng10@gmail.com>
2026-02-23 22:31:39 -0400

    Fix admin.py to show models on Backend GUI

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User,Department,Permit,Station,Region,ContactStation,Market,PersonalInfo,Contractor
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,15 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Department, db.session))
+    admin.add_view(ModelView(Permit, db.session))
+    admin.add_view(ModelView(Station, db.session))
+    admin.add_view(ModelView(Region, db.session))
+    admin.add_view(ModelView(ContactStation, db.session))
+    admin.add_view(ModelView(Market, db.session))
+    admin.add_view(ModelView(PersonalInfo, db.session))
+    admin.add_view(ModelView(Contractor, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
